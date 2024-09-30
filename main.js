247cea0a2a49d1f4234b8eb3ba70f052f1d061d3
kubgus <ja.gustafik@outlook.sk>
2024-09-30 08:56:14 +0200

    Add files

---
+++ main.js
@@ -0,0 +1,56 @@
+const DURATION = Number.parseInt(prompt("Timer seconds:", "300"));
+
+const timeElement = document.getElementById('time');
+const lineElement = document.getElementById('line');
+
+let running = false;
+let currentTime = DURATION;
+
+const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.abs(seconds % 60);
+    const formattedMinutes = minutes < 0 ? `-${minutes + 1}` : minutes;
+    const formattedSeconds = remainingSeconds < 10 ? `0${remainingSeconds}` : remainingSeconds;
+    return `${formattedMinutes}:${formattedSeconds}`
+}
+
+setInterval(() => {
+    if (running) {
+        currentTime--;
+    }
+}, 1000);
+
+const startStopTimer = () => {
+    currentTime = DURATION;
+    running = !running;
+};
+
+document.addEventListener('keyup', (event) => { 
+    if (event.key === 'x') {
+        startStopTimer();
+    }
+});
+document.addEventListener('click', () => {
+    startStopTimer();
+});
+
+function update() {
+    timeElement.textContent = formatTime(currentTime);
+    lineElement.style.width = `${(currentTime / DURATION) * 100}%`;
+
+    if (currentTime <= 10) {
+        timeElement.style.color = 'red';
+        lineElement.style.backgroundColor = 'red';
+    }
+    else if (currentTime < DURATION / 6) {
+        timeElement.style.color = 'white';
+        lineElement.style.backgroundColor = 'yellow';
+    }
+    else {
+        timeElement.style.color = 'white';
+        lineElement.style.backgroundColor = 'green';
+    }
+
+    requestAnimationFrame(update);
+}
+update();
